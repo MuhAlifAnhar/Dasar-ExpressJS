c2b47401afa615523f4e07702e32f5d6e282ae17
Muh. Alif Anhar <sultankautsar21@gmail.com>
2026-01-06 06:21:36 +0800

    fix

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
 
-app.use(() => {
-    console.log('we got request')
+app.get('/', (req, res) => {
+    res.send('Hello world');
+})
+
+app.get('/cats', (req, res) => {
+    res.send('Meow!');
+})
+
+app.get('/about', (req, res) => {
+    res.send('About page');
+})
+
+app.get(/.*/, (req, res) => {
+    res.send('404');
 });
+
+// app.use((req, res) => {
+//     res.send('404');
+// })
 
 app.listen(8080, () => {
     console.log('Server is running on http://localhost:8080');
